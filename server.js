85dd98c47721c8d35e0abc824347823848aff516
f-zahra <eva-200@hotmail.fr>
2025-02-11 14:36:56 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("welcome !");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
