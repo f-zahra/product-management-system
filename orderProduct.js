8d3df2c1ba6b5420d818051d2f4e7f2e936e338e
f-zahra <eva-200@hotmail.fr>
2025-02-13 19:20:06 -0500

    feat : create orderProduct model

---
+++ orderProduct.js
@@ -0,0 +1,13 @@
+// models/orderProduct.js
+const { DataTypes } = require("sequelize");
+const sequelize = require("./db");
+
+const OrderProduct = sequelize.define(
+  "OrderProduct",
+  {
+    quantity: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 }, // Quantity of the product
+  },
+  { schema: "product_management" }
+);
+
+module.exports = OrderProduct;
