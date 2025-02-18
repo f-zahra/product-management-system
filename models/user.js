de5ee1ff66e9e31b13f0ff231550513a072e3c14
f-zahra <eva-200@hotmail.fr>
2025-02-18 12:13:12 -0500

    refactor : add constraint in id field

--- models/user.js
+++ models/user.js
@@ -5,7 +5,12 @@ const sequelize = require("../db");
 const User = sequelize.define(
   "User",
   {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
   },
