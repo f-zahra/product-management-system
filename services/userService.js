c561adaf84865bb6f3eeb20e77a749f83bbb94ad
f-zahra <eva-200@hotmail.fr>
2025-03-19 15:58:57 -0400

    refactor: abstract transaction handling

--- services/userService.js
+++ services/userService.js
@@ -1,25 +1,31 @@
 class UserService {
-  constructor(userRepository) {
+  constructor(userRepository, transactionHandler) {
     this.userRepository = userRepository;
+    this.transactionHandler = transactionHandler;
   }
   //getuserByid
   async findUserById(userId) {
-    return this.userRepository.findUserById(userId);
+    return await this.userRepository.findUserById(userId);
   }
   //get all users
-  async findAllUsers({ limit, offset, order }) {
-    return this.userRepository.findAllUsers({ limit, offset, order });
+  async findAllUsers(queryOptions) {
+    return await this.userRepository.findAllUsers(queryOptions);
   }
   async createUser(name, email) {
-    return this.userRepository.saveNewUser(name, email);
+    //transaction is abstracted in case another db is implemented
+    return await this.transactionHandler(async (t) => {
+      const newUser = await this.userRepository.saveNewUser(name, email, t);
+      return newUser;
+    });
   }
+
   //update
   async updateUser(updatedUser, userId) {
-    return this.userRepository.updateUser(updatedUser, userId);
+    return await this.userRepository.updateUser(updatedUser, userId);
   }
   //delete
   async deleteUser(userId) {
-    return this.userRepository.deleteUser(userId);
+    return await this.userRepository.deleteUser(userId);
   }
 }
 
