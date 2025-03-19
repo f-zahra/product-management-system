34ad73ec5e038ee9e87dbe73f79f5879d0f73ebb
f-zahra <eva-200@hotmail.fr>
2025-03-19 16:01:59 -0400

    refactor: remove db dependency and apply error handling

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,45 +1,66 @@
-const User = require("./models/user");
-const sequelize = require("./db");
-
+const CustomError = require("../customError");
+//TODO :Implement repository interface for better abstraction and loose coupling
 class UserRepository {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
   //getuserByid
   async findUserById(userId) {
-    const user = await User.findByPk(userId);
+    const user = await this.userModel.findByPk(userId);
+    //if record not found
+    if (!user) {
+      // If no record is found, throw a custom error
+      const error = new CustomError("User not found", 404);
+      //passed to the error handler
+      throw error;
+    }
 
     return user;
   }
   //get all users
-  async findAllUsers({ limit, offset, order }) {
-    const users = await User.findAll({
-      limit: limit,
-      offset: offset,
-      order: [["name", order]],
-    });
+  //queryOptions : pagination and sorting
+  async findAllUsers(queryOptions = null) {
+    const users = await this.userModel.findAll(queryOptions);
     return users;
   }
-  async saveNewUser(name, email) {
-    //return a promise
-    return await sequelize.transaction(async (t) => {
-      const newUser = await User.create({ name, email }, { transaction: t });
-      return newUser.get({ plain: true }); // Return plain object
-    });
+  async saveNewUser(name, email, transaction = null) {
+    //find existing record first
+    const userRecord = await this.userModel.findOne({ where: { email } });
+    if (userRecord) {
+      const error = new CustomError(
+        "User with this email already exists.",
+        409
+      );
+      throw error;
+    } //return a promise
+    const newUser = await this.userModel.create(
+      { name, email },
+      { transaction }
+    );
+    return newUser.get({ plain: true }); // Return plain object
   }
   //update
   async updateUser(dataUpdated, userId) {
-    const updatedUser = User.update(dataUpdated, {
+    const updatedUser = await this.userModel.update(dataUpdated, {
       where: {
         id: userId,
       },
     });
+    if (updatedUser[0] === 0) {
+      throw new CustomError("User not found.", 404);
+    }
     return updatedUser;
   }
   //delete
   async deleteUser(userId) {
-    const deletedUser = await User.destroy({
+    const deletedUser = await this.userModel.destroy({
       where: {
         id: userId,
       },
     });
+    if (deletedUser === 0) {
+      throw new CustomError("User not found.", 404);
+    }
     return deletedUser;
   }
 }
