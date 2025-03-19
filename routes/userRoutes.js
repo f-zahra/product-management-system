fd64c964ed72665a78beebf4784e9931aec1ee6b
f-zahra <eva-200@hotmail.fr>
2025-03-19 16:01:01 -0400

    refactor: apply dependency injection for userService and userRepository

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,20 +2,35 @@ var express = require("express");
 const router = express.Router();
 
 const { validateUser } = require("../validators");
+const User = require("../models/user");
+const transactionHandler = require("../transactionHandler");
 
-const userController = require("../controllers/userController");
+const UserRepository = require("../repositories//userRepository");
+const UserService = require("../services/userService");
+const UserController = require("../controllers/userController");
 
+/**I have put the dependencies here because it's more simpler than putting in server.js */
+//call user service
+// Create instances of UserRepository and UserService
+const userRepository = new UserRepository(User);
+//inject repo into service for loose coupling
+const userService = new UserService(userRepository, transactionHandler);
+const userController = new UserController(userService);
 //Find user by id  (GET /users/:id)
-router.get("/:id", userController.getUserById);
+router.get("/:id", (req, res) => userController.getUserById(req, res));
 //Find all users  (GET /users)
-router.get("/", userController.getUsers);
+router.get("/", (req, res) => userController.getUsers(req, res));
 
 //Create a new user (POST /users)
-router.post("/", validateUser, userController.createUser);
+router.post("/", validateUser, (req, res) =>
+  userController.createUser(req, res)
+);
 
 //Update user (PUT /users/:d)
-router.put("/:id", validateUser, userController.updateUser);
+router.put("/:id", validateUser, (req, res) =>
+  userController.updateUser(req, res)
+);
 //Delete user (DELETE /users/:d)
-router.delete("/:id", userController.deleteUser);
+router.delete("/:id", (req, res) => userController.deleteUser(req, res));
 
 module.exports = router;
