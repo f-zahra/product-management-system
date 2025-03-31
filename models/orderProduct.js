3650ce5305b1495926269d91fe844a7d197a694e
f-zahra <eva-200@hotmail.fr>
2025-03-31 17:28:01 -0400

    fix: move association from orderProduct model to order model

--- models/orderProduct.js
+++ models/orderProduct.js
@@ -1,7 +1,6 @@
 // models/orderProduct.js
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db");
-
 const OrderProduct = sequelize.define(
   "OrderProduct",
   {
@@ -10,4 +9,14 @@ const OrderProduct = sequelize.define(
   { schema: "product_management" }
 );
 
+//MANY-TO-MANY
+Order.belongsToMany(Product, {
+  through: "OrderProducts", // Junction table name
+  foreignKey: "orderId",
+});
+//MANY-TO-MANY
+Product.belongsToMany(Order, {
+  through: "OrderProducts",
+  foreignKey: "product_id",
+});
 module.exports = OrderProduct;
