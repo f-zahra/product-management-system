0a5548a03a8af5720fe445600adf55b1a5b6f54c
f-zahra <eva-200@hotmail.fr>
2025-03-31 17:20:13 -0400

    feat : add error handling to crud operations

--- repositories/productRepository.js
+++ repositories/productRepository.js
@@ -1,12 +1,20 @@
+const CustomError = require("../customError");
+
 class ProductRepository {
   constructor(productModel) {
     this.productModel = productModel;
   }
 
   //findByid
-  async findByProductById(productId) {
+  async findProductById(productId) {
     const product = await this.productModel.findByPk(productId);
-    return product;
+    //if record not found
+    if (!product) {
+      const error = new CustomError("Product not found", 404);
+      //passed to error handler
+      throw error;
+    }
+    return product.get({ plain: true });
   }
   //findAll
   async findAllProducts(queryOptions = null) {
@@ -16,7 +24,7 @@ class ProductRepository {
 
   //create
   async saveNewProduct(productData, transaction = null) {
-    //find if record exist
+    //skip "find if record exist" for now because I havent implement any Unique fields
 
     const newProduct = await this.productModel.create(productData, {
       transaction,
@@ -26,11 +34,15 @@ class ProductRepository {
 
   //update
   async updateProduct(updatedData, productId) {
+    //skip "find if record exist" for now because I havent implement any Unique fields
     const updatedProduct = await this.productModel.update(updatedData, {
       where: {
         id: productId,
       },
     });
+    if (!updatedProduct) {
+      throw new CustomError("Product not found", 404);
+    }
     return updatedProduct;
   }
   //delete
@@ -38,6 +50,9 @@ class ProductRepository {
     const deletedProduct = await this.productModel.destroy({
       where: { id: productId },
     });
+    if (!deletedProduct) {
+      throw new CustomError("Product not found", 404);
+    }
     return deletedProduct;
   }
 }
