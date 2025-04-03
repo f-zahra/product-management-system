c98f939866063d4b6578a367bc1be2210a4c02e6
f-zahra <eva-200@hotmail.fr>
2025-04-03 18:40:59 -0400

    refactor : Modify user Model to include role field relates to #7

--- models/user.js
+++ models/user.js
@@ -16,6 +16,11 @@ const User = sequelize.define(
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
+    role: {
+      type: DataTypes.ENUM,
+      values: ["user", "admin"],
+      defaultValue: "user",
+    },
   },
   { schema: "product_management" }
 );
